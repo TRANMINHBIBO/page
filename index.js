1cae14665880422cb83956c36f07b33a1cb51cf2
TRANMINHBIBO <minhtran2632003@gmail.com>
2023-09-25 15:34:45 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 require("dotenv").config();
+const mongoose = require('mongoose');
 const port = process.env.PORT;
 const Router = require("./routers/client/index.router");
+const routerAdmin = require("./routers/admin/index.router");
+const database = require("./config/database");
 Router(app);
+routerAdmin(app);
+database.connect();
 app.set("views", "./views");
 app.set("view engine", "pug");
 app.use(express.static('public'))
