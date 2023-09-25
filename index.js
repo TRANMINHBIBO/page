c42e79d77ecbbf1cdf04d2fbd81ef744c277a861
TRANMINHBIBO <minhtran2632003@gmail.com>
2023-09-25 13:42:59 +0700

    cau hinh folder theo mvc

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello word");
+const express = require('express');
+const app = express();
+const port = 3000;
+const Router = require("./routers/client/index.router");
+Router(app);
+app.set("views", "./views");
+app.set("view engine", "pug");
+app.listen(port, () => {
+    console.log(`App listening port ${port}`);
+})
