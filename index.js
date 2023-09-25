84036c702b36f749bfd6963cdfdd01074141c733
TRANMINHBIBO <minhtran2632003@gmail.com>
2023-09-25 14:08:14 +0700

    cau truc folder pug

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+require("dotenv").config();
+const port = process.env.PORT;
 const Router = require("./routers/client/index.router");
 Router(app);
 app.set("views", "./views");
 app.set("view engine", "pug");
+app.use(express.static('public'))
 app.listen(port, () => {
     console.log(`App listening port ${port}`);
 })
